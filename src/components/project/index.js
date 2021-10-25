edab57e4e945a3eb8cdd69f2033b6cc74da8e598
andrehideki <andre.hkiw@gmail.com>
2021-10-25 17:44:51 -0300

    add: styled-components lib

--- src/components/project/index.js
+++ src/components/project/index.js
@@ -1,7 +1,21 @@
 export function Project() {
   return (
     <div>
-      
+      <header>
+        <h1>Nome</h1>
+      </header>
+      <main>
+        <section>
+          <div>
+            <label>Branch</label>
+            <select>
+              <option>master</option>
+              <option>h2</option>
+            </select>
+          </div>
+          <button>Gerar versão</button>
+        </section>
+      </main>
     </div>
   )
 }
